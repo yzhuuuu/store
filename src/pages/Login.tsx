bbf9f74f9e5dc9cf4bbce926615dd8a3d67214c8
colley zhu <zhucolley@outlook.com>
2024-05-08 21:24:08 -0400

    finish login and register pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,26 @@
+import { Form, Link } from "react-router-dom";
+import FormInput from "@/components/FormInput.tsx";
+import { Button } from "@/components/ui/button.tsx";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <main className={"grid place-items-center h-screen"}>
+      <Form className={"bg-accent px-8 py-8 rounded-lg grid gap-y-4 w-96"}>
+        <h2 className={"text-center text-2xl"}>Login</h2>
+        <FormInput labelText={"Email"} type={"text"} name={"email"} />
+        <FormInput labelText={"Password"} type={"password"} name={"password"} />
+
+        <Button type={"submit"}>Login</Button>
+
+        <p className={"text-center"}>
+          Not a member yet?{" "}
+          <span className={"text-primary font-semibold"}>
+            <Link to={"/register"}>Register</Link>
+          </span>
+        </p>
+      </Form>
+    </main>
+  );
 }
 
 export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,25 @@
+import { Form, Link } from "react-router-dom";
+import FormInput from "@/components/FormInput.tsx";
+import { Button } from "@/components/ui/button.tsx";
+
 function Register() {
-  return <div>Register</div>;
+  return (
+    <main className={"grid place-items-center h-screen"}>
+      <Form className={"bg-accent px-8 py-8 rounded-lg grid gap-y-4 w-96"}>
+        <h2 className={"text-center text-2xl"}>Register</h2>{" "}
+        <FormInput labelText={"User name"} type={"text"} name={"username"} />
+        <FormInput labelText={"Email"} type={"text"} name={"email"} />
+        <FormInput labelText={"Password"} type={"password"} name={"password"} />
+        <Button type={"submit"}>Login</Button>
+        <p className={"text-center"}>
+          Already a member?
+          <span className={"text-primary font-semibold text-sm ml-2"}>
+            <Link to={"/login"}>Login</Link>
+          </span>
+        </p>
+      </Form>
+    </main>
+  );
 }
 
 export default Register;
